64964f8c17e20aee02e634bb66a7549d99f7e1ce
DillenGrant <dillen.grant72@gmail.com>
2024-02-13 20:05:37 -0700

    components

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,10 +1,13 @@
-import { useStoreContext } from "../../utils/GlobalState";
+import React from 'react';
+import { useDispatch } from "react-redux";
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
 
 const CartItem = ({ item }) => {
 
-  const [, dispatch] = useStoreContext();
+ 
+
+  const dispatch = useDispatch();
 
   const removeFromCart = item => {
     dispatch({
